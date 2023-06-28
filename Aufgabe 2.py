8c0aecceb54d18921c0b01d2005b39caa464f615
TipstR <96343885+TipstR@users.noreply.github.com>
2023-06-28 16:16:50 +0200

    e

--- Aufgabe 2.py
+++ Aufgabe 2.py
@@ -15,6 +15,17 @@ class Block:
     def rel(self):
         return self.reliability
 
+    def make_graph(self, graph, origin=None):
+        if type(origin) == ParBlock:
+            for i in range(len(origin.blocks)):
+                graph.edge(origin.blocks[i].name, self.name)
+            return graph
+
+        if origin is not None:
+            graph.edge(origin.name, self.name)
+
+        return graph
+
 
 class SeqBlock:
     def __init__(self, name):
@@ -39,11 +50,21 @@ class SeqBlock:
 
         return reliability
 
+    def make_graph(self, graph, origin=None):
+        self.blocks[0].make_graph(graph, origin)
+        for i in range(len(self.blocks)-1):
+            self.blocks[i+1].make_graph(graph, self.blocks[i])
+
+        return graph
+
 
 class ParBlock:
     def __init__(self, name):
         self.blocks = []
         self.name = name
+
+    def __repr__(self):
+        return self.name
 
     def append(self, node):
         self.blocks.append(node)
@@ -56,31 +77,45 @@ class ParBlock:
         return self.name
 
     def rel(self):
-        reliability = 1.0
+        probability = 1.0
 
         for i in range(len(self.blocks)):
-            reliability = (1 - reliability) * (1 - self.blocks[i].rel())
+            probability *= 1 - self.blocks[i].rel()
 
-        reliability = 1 - reliability
+        return 1 - probability
 
-        return reliability
+    def make_graph(self, graph, origin):
+        if origin is not None:
+            for i in range(len(self.blocks)):
+                self.blocks[i].make_graph(graph, origin)
+        else:
+            pass
+
+        return graph
 
 
-e = Block("Eingang", 0.99)
-r1 = Block("Rechner1", 0.99)
-r2 = Block("Rechner2", 0.99)
-a = Block("Ausgang", 0.99)
+e = Block("eingang 0.99", 0.99)
+r1 = Block("r1 0.99", 0.99)
+r2 = Block("r2 0.99", 0.99)
+r3 = Block("r3 0.99", 0.99)
+r4 = Block("r4 0.99", 0.99)
+a = Block("ausgang 0.99", 0.99)
 
 seq = SeqBlock("Alle")
 par = ParBlock("Alle_Rechner")
 
 par.append(r1)
 par.append(r2)
+par.append(r3)
+par.append(r4)
 
 seq.append(e)
 seq.append(par)
 seq.append(a)
 
-g = graphviz.Graph("rel_diagram", graph_attr={"rankdir": "LR"}, node_attr={"shape":"rectangle"})
+g = graphviz.Graph("rel_diagram", graph_attr={"rankdir": "LR"}, node_attr={"shape": "rectangle"})
+
+g = seq.make_graph(g)
+g.view()
 
 print(seq.rel())
